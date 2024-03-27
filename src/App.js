2955e8fdcf7f26e9b931c9fe9ba05f391aa21a9b
mehmet dogan <mehmetseymen91@gmail.com>
2024-03-27 23:47:47 +0300

    state

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React, {useState} from 'react';
 import Categories from './Categories';
 import items from "./data"
+import Menu from "./Menu"
+
 
 function App() {
+  const [menuItems,setMenuItems] = useState(items)
+  const [activeCategory,setActiveCategory] = useState("")
+  const [category,setcCategory] = useState()
   return (
     <div className="App">
      
